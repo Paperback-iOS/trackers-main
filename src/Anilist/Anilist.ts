a3edcd23c047d7e11e3507c09bf1e3d3a75e36ee
IntermittentlyRupert <4526480+IntermittentlyRupert@users.noreply.github.com>
2022-05-07 15:22:21 +1000

    bump version

--- src/Anilist/Anilist.ts
+++ src/Anilist/Anilist.ts
@@ -38,7 +38,7 @@ export const AnilistInfo: SourceInfo = {
     author: 'Faizan Durrani',
     contentRating: ContentRating.EVERYONE,
     icon: 'icon.png',
-    version: '1.0.9',
+    version: '1.0.10',
     description: 'Anilist Tracker',
     authorWebsite: 'faizandurrani.github.io',
     websiteBaseURL: 'https://anilist.co'
